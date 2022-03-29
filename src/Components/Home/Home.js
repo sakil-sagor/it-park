522783f9777f631058858a13908f122d7c5ee066
sakil-sagor <sakilsagor76543@gmail.com>
2022-03-29 12:57:03 +0600

    services,teachers,contact us section added

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,5 +1,7 @@
 import React from 'react';
+import ServicesHome from '../ServicesHome/ServicesHome';
 import Slider from '../Slider/Slider';
+import TeachersHome from '../TeachersHome/TeachersHome';
 
 const Home = () => {
     return (
@@ -7,6 +9,10 @@ const Home = () => {
             <div className=" ">
                 {/* hero area  */}
                 <Slider></Slider>
+                {/* service area from services  */}
+                <ServicesHome></ServicesHome>
+                {/* Teacher area from teachers  */}
+                <TeachersHome></TeachersHome>
 
             </div>
         </div>
